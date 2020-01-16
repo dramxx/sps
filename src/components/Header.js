9c30b03ab8e06052bfe87de10ed675e93103d704
Drahos Madar <drahoslav.madar@aliter.com>
2020-01-16 12:37:04 +0100

    Netlify test

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,29 +8,29 @@ const Header = (props) => (
         <div className="logo">
 
             <span className="image main"><img src={logo} alt="" className="logo" /></span>
-            <span className="sps_logo"></span>
+            <span className="sps_logo"/>
 
         </div>
         <div className="content">
             <div className="inner">
-                {<h1>
+                <h1>
                     <span id="heading_1">Slovenská </span>
                     <span id="heading_2">pirátska </span>
                     <span id="heading_3">strana</span>
-                </h1>}
-                <p></p>
-                {<p id="subheading">S VAMI NA JEDNEJ LODI.</p>}
+                </h1>
+                <p id="subheading">S VAMI NA JEDNEJ LODI.</p>
+                <p id="subheading">test</p>
             </div>
         </div>
 
         <nav className="main_nav">
             <ul>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('intro') }}>PIRÁTI</a></li>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('work') }}>PROGRAM</a></li>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('about') }}>ĽUDIA</a></li>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('bepirate') }}>#BUDPIRAT</a></li>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('change') }}>#ZMENA</a></li>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('contact') }}>KONTAKT</a></li>
+                <li><a href="javascript:" onClick={() => { props.onOpenArticle('intro') }}>PIRÁTI</a></li>
+                <li><a href="javascript:" onClick={() => { props.onOpenArticle('work') }}>PROGRAM</a></li>
+                <li><a href="javascript:" onClick={() => { props.onOpenArticle('about') }}>ĽUDIA</a></li>
+                <li><a href="javascript:" onClick={() => { props.onOpenArticle('bepirate') }}>#BUDPIRAT</a></li>
+                <li><a href="javascript:" onClick={() => { props.onOpenArticle('change') }}>#ZMENA</a></li>
+                <li><a href="javascript:" onClick={() => { props.onOpenArticle('contact') }}>KONTAKT</a></li>
             </ul>
         </nav>
     </header>
